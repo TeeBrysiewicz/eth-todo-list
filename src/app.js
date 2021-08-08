a1e23de6c0ab60674e432dff86bd46e55ce38f9e
Tobias Brysiewicz <tbrysiewicz@att.net>
2021-08-08 13:24:09 -0500

    list task and tests

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 App = {
-
+  loading: false,
   contracts: {},
 
   load: async () => {
@@ -44,7 +44,6 @@ App = {
     // Create a JavaScript version of the smart contract
     const todoList = await $.getJSON('TodoList.json')
     console.log(todoList)
-    // var contract = require("@truffle-contract");
     App.contracts.TodoList = await TruffleContract(todoList)
     App.contracts.TodoList.setProvider(App.provider)
 
@@ -53,8 +52,66 @@ App = {
   },
 
   render: async () => {
+    // Prevent double render
+    if (App.loading) {
+      return
+    }
+
+    // Update app loading state
+    App.setLoading(true)
+
     // Render Account
     $('#account').html(App.account)
+
+    // Render Tasks
+    await App.renderTasks()
+
+    // Update loading state
+    App.setLoading(false)
+  },
+
+  renderTasks: async () => {
+    // Load the total task count from the blockchain
+    const taskCount = await App.todoList.taskCount()
+    const $taskTemplate = $('.taskTemplate')
+
+    // Render out each task with a new task template
+    for (var i = 1; i <= taskCount; i++) {
+      const task = await App.todoList.tasks(i)
+      const taskId = task[0].toNumber()
+      const taskContent = task[1]
+      const taskCompleted = task[2]
+
+      const $newTaskTemplate = $taskTemplate.clone()
+      $newTaskTemplate.find('.content').html(taskContent)
+      $newTaskTemplate.find('input')
+                      .prop('name', taskId)
+                      .prop('checked', taskCompleted)
+                      // .on('click', App.toggleCompleted)
+
+      // Put the task in the correct list
+      if (taskCompleted) {
+        $('#completedTaskList').append($newTaskTemplate)
+      } else {
+        $('#taskList').append($newTaskTemplate)
+      }
+
+      // Show the task
+      $newTaskTemplate.show()
+    }
+  },
+
+  setLoading: (boolean) => {
+    App.loading = boolean
+    const loader = $('#loader')
+    const content = $('#content')
+    if (boolean) {
+      loader.show()
+      content.hide()
+    } else {
+      loader.hide()
+      content.show()
+    }
   }
 }
 
